7a5c78b1ed3a4e95aedd17b7ec833b0449850888
JagadeeshKJ <70704500+JagadeeshKJ@users.noreply.github.com>
2021-12-13 22:05:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const header = document.querySelector('#header')
+const title = document.querySelector('#title')
+const excerpt = document.querySelector('#excerpt')
+const profile_img = document.querySelector('#profile_img')
+const name = document.querySelector('#name')
+const date = document.querySelector('#date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 3000)
+
+function getData() {
+    header.innerHTML = '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
+    title.innerHTML = 'Lorem ipsum dolor sit amet.'
+    excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, iste!'
+    profile_img.innerHTML = '<img src="https:randomuser.me/api/portraits/women/65.jpg" alt="">'
+    name.innerHTML = 'Mia Smoak'
+    date.innerHTML = 'Dec 10th, 2021'
+
+    animated_bgs.forEach(bg => bg.classList.remove('animated-bg'))
+    animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-text'))
+}
+
