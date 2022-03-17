b31239a81ddbcc179a5166f46fe7bdc7f9d33324
Jagadeesh kj <70704500+JagadeeshKJ@users.noreply.github.com>
2022-03-17 13:17:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ setTimeout(getData, 3000)
 
 function getData() {
     header.innerHTML = '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
-    title.innerHTML = 'Lorem ipsum dolor sit amet.'
-    excerpt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, iste!'
+    title.innerHTML = 'Hello There!'
+    excerpt.innerHTML = 'Experience this cool content placeholder! Simple & Dope.'
     profile_img.innerHTML = '<img src="https://pm1.narvii.com/7309/b8b859b99ca7a565c464c4193d9cc9f7d36ce3ear1-250-250v2_128.jpg" alt="">'
     name.innerHTML = 'Mia Smoak'
     date.innerHTML = 'Dec 10th, 2021'
